e8c4ba0b3adf418881c0f1ca8479fb9b43ce8526
eav1stfr <eavst123@gmail.com>
2024-12-08 19:06:50 +0500

    setup the project

---
+++ StocksApp/StocksApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StocksApp
+//
+//  Created by Александр Эм on 08.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
